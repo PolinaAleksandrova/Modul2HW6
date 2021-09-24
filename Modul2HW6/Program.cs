54611cfea83632678052df672ce0fe227cb96c9a
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-09-24 18:54:06 +0300

    Add project files.

---
+++ Modul2HW6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul2HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
